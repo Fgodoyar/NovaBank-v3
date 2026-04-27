a9476917a8fb8f5174befd405e789d59932e15d4
Francisco Godoy Arenas <fgodoyar@emeal.nttdata.com>
2026-04-27 17:40:00 +0200

    feature: Proyecto base

---
+++ src/main/java/com/nov/novabank_v3/NovabankV3Application.java
@@ -0,0 +1,13 @@
+package com.nov.novabank_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovabankV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovabankV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nov/novabank_v3/NovabankV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nov.novabank_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovabankV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
