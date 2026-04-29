1547a2c72bc1b55bcdbe2d530fb663c3d6b0fa0f
Francisco Godoy Arenas <fgodoyar@emeal.nttdata.com>
2026-04-29 10:00:35 +0200

    refactor: Se han añadido las etiquetas @Repository faltantes

--- src/main/java/com/nov/novabank_v3/repository/AccountRepository.java
+++ src/main/java/com/nov/novabank_v3/repository/AccountRepository.java
@@ -4,10 +4,12 @@ import com.nov.novabank_v3.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByAccountNumber(String accountNumber);
     List<Account> findByCustomerId(Long customerId);
--- src/main/java/com/nov/novabank_v3/repository/TransactionRepository.java
+++ src/main/java/com/nov/novabank_v3/repository/TransactionRepository.java
@@ -2,10 +2,12 @@ package com.nov.novabank_v3.repository;
 
 import com.nov.novabank_v3.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findByAccountIdOrderByCreationDateDesc(Long accountId);
     List<Transaction> findByAccountIdAndCreationDateBetweenOrderByCreationDateDesc(Long accountId, LocalDateTime startDate, LocalDateTime endDate);
